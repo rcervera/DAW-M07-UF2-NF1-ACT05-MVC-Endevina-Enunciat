34de35289fb2c1b1cdd3dc142d341c1504c572fe
rcervera <rcerver4@xtec.cat>
2020-11-30 08:47:40 +0000

    Update principal.php

--- vistes/default/principal.php
+++ vistes/default/principal.php
@@ -3,6 +3,6 @@
 	  <div class="jumbotron mt-2">
 		  <h1 class="display-4">Endevina un número</h1>
 		  <p class="lead">Aplicació per jugar a endevinar un número</p>
-		  <hr class="my-4"></hr>			  
+		  </hr>			  
 	  </div>
    </div>
